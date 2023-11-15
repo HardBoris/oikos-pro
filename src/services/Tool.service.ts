d9b649572a1022f76fbf5f760394cf07f30d1aac
HardBoris <bgaibor03@gmail.com>
2023-11-15 15:34:32 -0400

    feat: tool service

---
+++ src/services/Tool.service.ts
@@ -0,0 +1,75 @@
+import { Request } from "express";
+import { Tool } from "../entities/Element";
+import { ErrorHandler } from "../errors";
+import { ToolRepository } from "../repositories";
+
+class ToolService {
+  ToolCreator = async (req: Request): Promise<any> => {
+    const body = req.body;
+    console.log(body);
+    const { element } = body;
+    const buscado = await ToolRepository.findOne({ element: element });
+
+    if (buscado) {
+      return buscado;
+    } else {
+      const cosa: Tool = await ToolRepository.save({
+        ...body,
+      });
+
+      return cosa;
+    }
+  };
+
+  ToolsLoader = async (req: Request) => {
+    let stuffs: Tool[] = await ToolRepository.all();
+    /* stuffs = stuffs.sort((a, b) =>
+      a.stuffId > b.stuffId ? -1 : a.stuffId < b.stuffId ? 1 : 0
+    ); */
+    return {
+      status: 200,
+      stuffs: stuffs,
+    };
+  };
+
+  ToolLoader = async (req: Request) => {
+    const stuff: Tool = await ToolRepository.findOne({
+      stuffId: req.params.stuffId,
+    });
+    return { status: 200, stuff: stuff };
+  };
+
+  ToolEditor = async (req: Request) => {
+    const stuff: Tool = await ToolRepository.findOne({
+      stuffId: req.params.stuffId,
+    });
+    const stuffUpdated = {
+      ...stuff,
+      stuff: req.body.stuff,
+    };
+    await ToolRepository.save(stuffUpdated);
+    return {
+      status: 200,
+      stuff: stuffUpdated,
+    };
+  };
+
+  ToolDeletor = async (req: Request) => {
+    const stuff: Tool = await ToolRepository.findOne({
+      stuffId: req.params.stuffId,
+    });
+
+    if (!stuff) {
+      throw new ErrorHandler(404, "Tool not found");
+    }
+
+    await ToolRepository.delete(req.params.stuffId);
+
+    return {
+      status: 200,
+      message: "Tool deleted",
+    };
+  };
+}
+
+export default new ToolService();
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import ElementService from "./Element.service";
 import StuffService from "./Stuff.service";
 import MidiaService from "./Midia.service";
+import ToolService from "./Tool.service";
 
-export { ElementService, StuffService, MidiaService };
+export { ElementService, StuffService, MidiaService, ToolService };
