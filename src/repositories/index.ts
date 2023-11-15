e181cb280496103b1ff5607679236988ad42e541
HardBoris <bgaibor03@gmail.com>
2023-11-15 17:27:00 -0400

    feat: purchase request repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,5 +2,12 @@ import ElementRepository from "./Element.repository";
 import StuffRepository from "./Stuff.repository";
 import MidiaRepository from "./Midia.repository";
 import ToolRepository from "./Tool.repository";
+import PurchaseRequestRepository from "./PurchaseRequest.repository";
 
-export { ElementRepository, StuffRepository, MidiaRepository, ToolRepository };
+export {
+  ElementRepository,
+  StuffRepository,
+  MidiaRepository,
+  ToolRepository,
+  PurchaseRequestRepository,
+};
